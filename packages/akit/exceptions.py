85156051dfd6c4ce30d9a9ec4ff65617dbc14b7a
Myron Walker <myron.walker@gmail.com>
2020-03-15 18:24:53 -0700

    Did some re-organization of the exception types.

--- packages/akit/exceptions.py
+++ packages/akit/exceptions.py
@@ -19,45 +19,82 @@ class AKitError(Exception):
         The base error object for Automation Kit errors.
     """
 
-class AKitSemanticError(AKitError):
+# ==================================================================================
+#                            BASE ERROR CLASSIFICATIONS
+# ==================================================================================
+
+class AKitConfigurationError(AKitError):
     """
-        The base error object for Automation Kit errors.
+        The base error object for errors that indicate that there is an issue related
+        to improper configuration.
     """
 
-class AKitInitialConnectivityError(AKitError):
+class AKitLandscapeError(AKitError):
+    """
+        The base error object for errors that indicate that there is an issue related
+        to the interaction usage or consumption of an environmental resources.
+    """
+
+class AKitRuntimeError(AKitError):
+    """
+        The base error object for errors that indicate that an error was produced during
+        the execution of task or test code and the error was not able to be classified
+        as Configuration, Landscape, or Semantic related.
+    """
+
+class AKitSemanticError(AKitError):
+    """
+        The base error object for errors that indicate that there is an issue with
+        a piece of automation code and with the way the Automation Kit code is being
+        utilized.  
+    """
+
+# ==================================================================================
+#                         CONFIGURATION RELATED ERRORS
+# ==================================================================================
+
+class AKitInvalidConfigError(AKitConfigurationError):
+    """
+        This error is raised when an IntegrationMixIn object has been passed invalid configuration parameters.
+    """
+
+class AKitMissingConfigError(AKitConfigurationError):
+    """
+        This error is raised when an IntegrationMixIn object is missing required configuration parameters.
+    """
+
+
+# ==================================================================================
+#                           LANDSCAPE RELATED ERRORS
+# ==================================================================================
+class AKitInitialConnectivityError(AKitLandscapeError):
     """
         This error is raised when an IntegrationMixIn object is unable to establish an initial level of
         connectivity with a connected resource.
     """
 
-class AKitInvalidConfigError(AKitError):
+class AKitResourceError(AKitLandscapeError):
     """
-        This error is raised when an IntegrationMixIn object has been passed invalid configuration parameters.
-    """
-
-class AKitMissingConfigError(AKitError):
-    """
-        This error is raised when an IntegrationMixIn object is missing required configuration parameters.
+        This error is raised when an IntegrationMixIn object was unable to obtain a required resource.
     """
 
-class AKitOutOfScopeError(AttributeError):
+# ==================================================================================
+#                           RUNTIME RELATED ERRORS
+# ==================================================================================
+
+class AKitOutOfScopeError(AKitRuntimeError):
     """
         This error is raised when a method is called on a ScopeMixIn that is not in scope.  A test can have,
         multiple ScopeMixIn(s) and can run in multiple scopes but the test must be instantiated and run in
         each scope individually.
     """
 
-class AKitResourceError(AKitError):
-    """
-        This error is raised when an IntegrationMixIn object was unable to obtain a required resource.
-    """
-
-class AKitScopeEntryError(AKitError):
+class AKitScopeEntryError(AKitRuntimeError):
     """
         This error is raised when a ScopeMixIn was unable to complete the entry of a scope.
     """
 
-class AKitRequestStopError(AKitError):
+class AKitRequestStopError(AKitRuntimeError):
     """
         This error is raised when a test indicates it wants to stop an automation run.  The `TestSequencer`
         may or may not stop the automation run as a result of a test or scope raising this error.  The
@@ -65,7 +102,7 @@ class AKitRequestStopError(AKitError):
         and will stop the test run if the runtime context indicates that stopping is allowed.
     """
 
-class AKitSkipError(AKitError):
+class AKitSkipError(AKitRuntimeError):
     """
         This error is raised when a test indicates it wants to be skipped while being run
     """
@@ -75,3 +112,6 @@ class AKitSkipError(AKitError):
         return
 
 
+# ==================================================================================
+#                           SEMANTIC RELATED ERRORS
+# ==================================================================================
